85c7eb309938e3be680a3bd30d78509a367ec7e2
Risto Yrjänä <risto.yrjana@vaadin.com>
2013-05-07 07:37:08 +0300

    Add pushmode handling

--- addon/src/test/scala/vaadin/scala/tests/UITests.scala
+++ addon/src/test/scala/vaadin/scala/tests/UITests.scala
@@ -42,15 +42,17 @@ class UITests extends ScaladinTestSuite {
     assert(ui.widgetset === None)
     assert(ui.preserveOnRefresh === false)
     assert(ui.p != null)
+    assert(ui.pushMode == PushMode.Disabled)
 
     val wrappedVaadinUI = new WrappedVaadinUI
-    ui = new UI("mytitle", "mytheme", "mywidgetset", true, wrappedVaadinUI) {}
+    ui = new UI("mytitle", "mytheme", "mywidgetset", true, PushMode.Manual, wrappedVaadinUI) {}
 
     assert(ui.title === Some("mytitle"))
     assert(ui.theme === "mytheme")
     assert(ui.widgetset === Some("mywidgetset"))
     assert(ui.preserveOnRefresh === true)
     assert(ui.p === wrappedVaadinUI)
+    assert(ui.pushMode == PushMode.Manual)
   }
 
   test("initialization without init(request: ScaladinRequest)") {
